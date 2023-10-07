c7e8bbbf9c3b3de3138c838d28b0aaa69c906933
Anakin Chen <anakinchen861101@gmail.com>
2023-10-07 19:22:49 +0800

    tree-sitter-hypr

---
+++ lua/custom/plugins/tree-sitter-hypr.lua
@@ -0,0 +1,14 @@
+return {
+    "luckasRanarison/tree-sitter-hypr",
+    config = function ()
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.hypr = {
+            install_info = {
+                url = "https://github.com/luckasRanarison/tree-sitter-hypr",
+                files = { "src/parser.c" },
+                branch = "master",
+            },
+            filetype = "hypr",
+        }
+    end
+}
